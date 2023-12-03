0461532b683484b766fe537d13f16a9b28014a9b
Kaylaine <85030686+Kaylaineasb@users.noreply.github.com>
2023-12-03 20:40:18 -0300

    testando login

---
+++ testando_login/app/screens/List.tsx
@@ -0,0 +1,20 @@
+import { NavigationProp } from '@react-navigation/native'
+import React from 'react'
+import {FIREBASE_AUTH} from '../../FirebaseConfig'
+import { Button, View } from 'react-native';
+
+
+interface RouterProps{
+    navigation: NavigationProp<any,any>;
+}
+
+const List = ({navigation}:RouterProps) => {
+  return (
+    <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+        <Button onPress={()=>navigation.navigate('details')} title='Open Details'/>
+        <Button onPress={()=>FIREBASE_AUTH.signOut()} title='Logout'/>
+    </View>
+  )
+}
+
+export default List
